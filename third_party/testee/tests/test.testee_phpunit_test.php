35bdc5f6fcd30ae92db21ffb4d58d79867f116a7
Greg Ferrell <info@studiokoi.com>
2013-01-22 22:53:22 -0500

    fixed test item to have each test in a separate function the PHPUnit way.

--- third_party/testee/tests/test.testee_phpunit_test.php
+++ third_party/testee/tests/test.testee_phpunit_test.php
@@ -2,9 +2,18 @@
 
 class Testee_phpunit_test extends Testee_phpunit_test_case
 {
-	public function testMessage()
+	public function test__db__mock_methods_return_self()
+	{
+		$this->assertEquals(
+			get_class($this->EE->db->select()),
+			'Testee_phpunit_test_mock_db',
+			'Mocked DB functions should return themselves.'
+		);
+	}
+
+
+	public function test__fail__on_purpose()
 	{
 		$this->assertTrue(FALSE, 'This is failing on purpose.');
-		$this->assertEquals(get_class($this->EE->db->select()), 'Mod_freeform_phpunit_mock_db', 'should be equal');
 	}
 }
