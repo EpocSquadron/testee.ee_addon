c5ae2ed4afed47fee26d84c074e177233d078a79
Greg Ferrell <info@studiokoi.com>
2013-01-24 00:58:56 -0500

    added test for mockery

---
+++ third_party/testee/tests/test.testee_phpunit_mockery_test.php
@@ -0,0 +1,35 @@
+<?php
+
+class Testee_phpunit_mockery_test extends Testee_phpunit_test_case
+{
+	public function setUp()
+	{
+		//use mockery
+		parent::setUp(true);
+	}
+
+	public function test__db__mock_methods_return_self()
+	{
+		$this->assertEquals(
+			'Testee_phpunit_test_mock_db',
+			get_class($this->EE->db->select()),
+			'Mocked DB functions should return themselves.'
+		);
+	}
+
+	public function test__db__getting_fresh_methods_works()
+	{
+		$tester = $this->getMockByName('db', true);
+
+		$this->assertEquals(
+			'Testee_phpunit_test_mock_db',
+			get_class($tester->select()),
+			'Mocked DB functions should return themselves.'
+		);
+	}
+
+	public function test__fail__on_purpose()
+	{
+		$this->assertTrue(FALSE, 'This is failing on purpose.');
+	}
+}
