f92daff67346256e91a0e6cae66d1b6d28d951a6
Greg Ferrell <info@studiokoi.com>
2013-01-24 01:34:20 -0500

    fixed mockery availability unit test to correctly test for results

--- third_party/testee/tests/test.testee_phpunit_mockery_test.php
+++ third_party/testee/tests/test.testee_phpunit_mockery_test.php
@@ -11,25 +11,20 @@ class Testee_phpunit_mockery_test extends Testee_phpunit_test_case
 	public function test__db__mock_methods_return_self()
 	{
 		$this->assertEquals(
-			'Testee_phpunit_test_mock_db',
+			get_class($this->EE->db),
 			get_class($this->EE->db->select()),
 			'Mocked DB functions should return themselves.'
 		);
 	}
 
-	public function test__db__getting_fresh_methods_works()
+	public function test__db__getting_fresh_mocks_work()
 	{
 		$tester = $this->getMockByName('db', true);
 
-		$this->assertEquals(
-			'Testee_phpunit_test_mock_db',
-			get_class($tester->select()),
-			'Mocked DB functions should return themselves.'
+		$result = get_class($tester->select());
+		$this->assertTrue(
+			!!stristr($result, 'mockery'),
+			'New mocks should be of class Mockery. Recieved: ' . $result
 		);
-	}
-
-	public function test__fail__on_purpose()
-	{
-		$this->assertTrue(FALSE, 'This is failing on purpose.');
 	}
 }
